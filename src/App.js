a5c34d230f371a282f957cceb48c52d7e35b9b00
AshishZiniya007 <ashishziniya1234@gmail.com>
2023-03-02 22:23:25 +0530

    Day 3

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 import Greet from "./component/Greet";
 import Welcome from "./component/Welcome";
 import Hello from './component/Hello';
+import Message from './component/Message';
+import Counter from './component/Counter';
 
 function App() {
   return (
     <div className="App">
-        <Greet name="Bruce" heroName="Batman">
-          <p>This is children props</p></Greet>
-        <Greet name="Clark" heroName="Superman">
-          <button>Action</button>
-        </Greet>
-        <Greet name="Diana" heroName="Wonder Woman"/>
-        <Welcome name="Bruce" heroName="Batman"/>
-        <Welcome name="Clark" heroName="Superman"/>
-        <Welcome name="Diana" heroName="Wonder Woman"/>
-        {/* <Hello/> */}
+      {/* <Counter/> */}
+      {/* <Message/> */}
+      {/* <Greet name="Bruce" heroName="Batman"> */}
+      {/* <p>This is children props</p></Greet> */}
+      {/* <Greet name="Clark" heroName="Superman"> */}
+      {/* <button>Action</button> */}
+      {/* </Greet> */}
+      <Greet name="Diana" heroName="Wonder Woman"/>
+      <Welcome name="Bruce" heroName="Batman"/>
+      {/* <Welcome name="Clark" heroName="Superman"/> */}
+      {/* <Welcome name="Diana" heroName="Wonder Woman"/> */}
+      {/* <Hello/> */}
     </div>
   );
 }
---
+++ src/component/Counter.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    increament() {
+        // this.setState(
+        //     {
+        //         count: this.state.count + 1
+        //     },
+        //     () => {
+        //         console.log('callback value', this.state.count)
+        //     }
+        // )
+        
+        this.setState((prevState, props) =>({
+            count: prevState.count + 1
+        }))
+        console.log(this.state.count)
+    }
+
+    increamentFive() {
+        this.increament()
+        this.increament()
+        this.increament()
+        this.increament()
+        this.increament()
+    }
+
+    render() {
+        return (
+            <div>
+                <div>Count - {this.state.count}</div>
+                <button onClick={() => this.increamentFive()}>Increament</button>
+            </div>
+        )
+    }
+}
+
+export default Counter
