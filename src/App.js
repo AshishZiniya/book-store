0b85c8f1563c7a35f49934e5c7b1a8b30953f30f
AshishZiniya007 <ashishziniya1234@gmail.com>
2023-03-02 23:16:10 +0530

    Day 4

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import Welcome from "./component/Welcome";
 import Hello from './component/Hello';
 import Message from './component/Message';
 import Counter from './component/Counter';
+import FunctionClick from './component/FunctionClick';
+import ClassClick from './component/ClassClick';
+import EventBind from './component/EventBind';
+import ParentComponent from './component/ParentComponent';
 
 function App() {
   return (
     <div className="App">
+      <ParentComponent/>
+      {/* <EventBind/> */}
+      {/* <FunctionClick/> */}
+      {/* <ClassClick/> */}
       {/* <Counter/> */}
       {/* <Message/> */}
       {/* <Greet name="Bruce" heroName="Batman"> */}
@@ -16,8 +24,8 @@ function App() {
       {/* <Greet name="Clark" heroName="Superman"> */}
       {/* <button>Action</button> */}
       {/* </Greet> */}
-      <Greet name="Diana" heroName="Wonder Woman"/>
-      <Welcome name="Bruce" heroName="Batman"/>
+      {/* <Greet name="Diana" heroName="Wonder Woman"/> */}
+      {/* <Welcome name="Bruce" heroName="Batman"/> */}
       {/* <Welcome name="Clark" heroName="Superman"/> */}
       {/* <Welcome name="Diana" heroName="Wonder Woman"/> */}
       {/* <Hello/> */}
