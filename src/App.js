70db1edb1e41d2338524946866bf0413a3a02bc8
AshishZiniya007 <ashishziniya1234@gmail.com>
2023-03-07 19:02:28 +0530

    DAY 10 ALL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import ClickCounterTwo from './component/ClickCounterTwo';
-import Counter1 from './component/Counter1';
-import HoverCounterTwo from './component/HoverCounterTwo';
-// import User from './component/User';
-// import ClickCounter from './component/ClickCounter';
-// import HoverCounter from './component/HoverCounter';
-
+import ComponentC from './component/ComponentC';
+import { UserProvider } from './component/UserContext';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter1>{(count, increamentCount) => <ClickCounterTwo count={count} increamentCount={increamentCount}/>}</Counter1>
-        <Counter1>{(count, increamentCount) => <HoverCounterTwo count={count} increamentCount={increamentCount}/>}</Counter1>
-        {/* <ClickCounterTwo/>
-        <HoverCounterTwo/>
-        <User render={(isLoggedIn) => isLoggedIn ? 'Ashish' : 'Guest'}/> */}
-        {/* <ClickCounter name='Ashish'/>
-        <HoverCounter/> */}
+        <UserProvider value="Ashish">
+          <ComponentC />
+        </UserProvider>
       </div>
     );
   }
