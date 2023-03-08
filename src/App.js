2a6cc77dc0b40ca062655484c49bdd6dc705acc0
AshishZiniya007 <ashishziniya1234@gmail.com>
2023-03-08 19:49:47 +0530

    DAY 13

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import PostForm from './component/PostForm';
-// import PostList from './component/PostList';
+// import HookCounter from './component/HookCounter';
+import HookCounterTwo from './component/HookCounterTwo';
+// import ClassCounter from './component/ClassCounter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PostForm/>
-        {/* <PostList/> */}
+        <HookCounterTwo/>
+        {/* <ClassCounter/> */}
       </div>
     );
   }
