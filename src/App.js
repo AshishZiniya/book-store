7016b6d1dfa5b5d8dbab2ba0dbf8cd0f662fbb07
AshishZiniya007 <ashishziniya1234@gmail.com>
2023-03-09 11:54:45 +0530

    DAY 17

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-// import HookMouse from './component/HookMouse';
-import MouseContainer from './component/MouseContainer';
-// import ClassMouse from './component/ClassMouse';
-// import ClassComponentOne from './component/ClassComponentOne';
-// import HookCounterOne from './component/HookCounterOne';
-// import HookCounterFour from './component/HookCounterFour';
-// import HookCounterThree from './component/HookCounterThree';
+import DataFetching from './component/DataFetching';
+// import IntervalClassCounter from './component/IntervalClassCounter';
+// import IntervalHookCounter from './component/IntervalHookCounter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <HookMouse/> */}
-        <MouseContainer/>
-        {/* <ClassMouse/> */}
-        {/* <ClassComponentOne/> */}
-        {/* <HookCounterOne/> */}
-        {/* <HookCounterFour/> */}
-        {/* <HookCounterThree/> */}
+        <DataFetching/>
+        {/* <IntervalClassCounter/> */}
+        {/* <IntervalHookCounter/> */}
       </div>
     );
   }
