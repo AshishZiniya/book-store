7a904e868c70ff9e688f175d6ea2778c988b5eb1
AshishZiniya007 <ashishziniya1234@gmail.com>
2023-03-17 19:27:40 +0530

    DAY 20

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-import UserForm1 from './component/UserForm1';
-// import CounterOne1 from './component/CounterOne1';
-// import CounterTwo1 from './component/CounterTwo1';
-// import DocTitleOne from './component/DocTitleOne';
-// import DocTitleTwo from './component/DocTitleTwo';
+import UseState from './component/UseState';
 
 function App() {
   return (
     <div className="App">
-      <UserForm1/>
-      {/* <CounterOne1/> */}
-      {/* <CounterTwo1/> */}
-      {/* <DocTitleOne /> */}
-      {/* <DocTitleTwo /> */}
+     <UseState/>
     </div>
   );
 }
